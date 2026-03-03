059952db5621dc0bbffb97378d0c9930531f5b99
الاستاذ خليل البلوشي <omk456456@gmail.com>
2026-03-03 10:18:38 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const cacheName = 'khalil-app-v1';
-const assets = ['./', './index.html'];
+const CACHE_NAME = 'khalil-app-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800&display=swap'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// تثبيت ملف الخدمة وتخزين الملفات الأساسية
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// تفعيل وتحديث الـ Cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// الاستجابة في حال عدم وجود انترنت
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
